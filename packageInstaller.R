aaa150f364ec5f06e27c63bfe17ae688339e91b0
idoerg <idoerg@gmail.com>
2021-12-02 12:42:00 -0600

    modified:   packageInstaller.R Fixed so that Biostrings is from BiocManager

--- packageInstaller.R
+++ packageInstaller.R
@@ -1,4 +1,7 @@
-packages <- c("stringr", "stringi", "Biostrings", "rentrez", "rlist", "plyr", "Rcpp", "curl", "httr", "jsonlite", "xml2")
+
+#packages <- c("stringr", "stringi", "Biostrings", "rentrez", "rlist", "plyr", "Rcpp", "curl", "httr", "jsonlite", "xml2")
+
+packages <- c("stringr", "stringi", "rentrez", "rlist", "plyr", "Rcpp", "curl", "httr", "jsonlite", "xml2")
 
 # Install packages not yet installed
 installed_packages <- packages %in% rownames(installed.packages())
@@ -8,3 +11,8 @@ if (any(installed_packages == FALSE)) {
 
 # Packages loading
 invisible(lapply(packages, library, character.only = TRUE))
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install("Biostrings")
