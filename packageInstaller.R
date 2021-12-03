ed798002ff9e89027068070b0a04cd978642c049
Parnal Joshi <44910482+parnaljoshi@users.noreply.github.com>
2021-12-02 23:23:00 -0600

    Update packageInstaller.R

--- packageInstaller.R
+++ packageInstaller.R
@@ -1,3 +1,6 @@
+# create local user library path (not present by default)
+dir.create(path = Sys.getenv("R_LIBS_USER"), showWarnings = FALSE, recursive = TRUE)
+
 #packages <- c("stringr", "stringi", "Biostrings", "rentrez", "rlist", "plyr", "Rcpp", "curl", "httr", "jsonlite", "xml2")
 packages <- c("stringr", "stringi", "BiocManager", "rentrez", "rlist", "plyr", "Rcpp", "curl", "httr", "jsonlite", "xml2")
 
@@ -5,9 +8,9 @@ packages <- c("stringr", "stringi", "BiocManager", "rentrez", "rlist", "plyr", "
 # Install packages not yet installed
 installed_packages <- packages %in% rownames(installed.packages())
 if (any(installed_packages == FALSE)) {
-  install.packages(packages[!installed_packages])
+  install.packages(packages[!installed_packages], lib = Sys.getenv("R_LIBS_USER"))
 }
-BiocManager::install("Biostrings")
+BiocManager::install("Biostrings", lib = Sys.getenv("R_LIBS_USER"))
 
 # Packages loading
 invisible(lapply(packages, library, character.only = TRUE))
